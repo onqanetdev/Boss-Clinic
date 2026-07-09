2febc99490def1eb2d10ff8b0684389d496047db
Faizan Khan <onqanetdev@gmail.com>
2026-07-09 11:01:23 +0530

    Notification Added ✅

--- Boss Clinic/Boss_ClinicApp.swift
+++ Boss Clinic/Boss_ClinicApp.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+
+
+
 
 @main
 struct Boss_ClinicApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             //ContentView()
